9e84ec0132319d614874326dea17815feb8efa30
Khactrung93 <81963134+Khactrung93@users.noreply.github.com>
2025-05-16 23:37:55 +1000

    Update CheckMildura.py

--- CheckMildura.py
+++ CheckMildura.py
@@ -28,36 +28,7 @@ headers = {
     "x-rap-xsrf-token": "AImk1AKXqN70Ab-sOEh-4yfWzEdgG3lcdg:1747391486296",
 }
 
-cookies = {
-    # Dán toàn bộ cookies lấy từ cURL vào đây, ví dụ:
-    # "RAP_XSRF_TOKEN": "...",
-    # ...
-    "RAP_XSRF_TOKEN": "AImk1AKXqN70Ab-sOEh-4yfWzEdgG3lcdg:1747391486296",
-    "HSID": "AQv__Nxsp_Dx8J3IP",
-    "SSID": "AzeIK_MGxEX2raqaX",
-    "APISID": "63ro8FeNlfHPCq0G/Ao82Q_iNRjhgPQ24k",
-    "SAPISID": "wn-_6ONd3KJeJEpJ/ATYLZrOKQQ03Cqu1t",
-    "__Secure-1PAPISID": "wn-_6ONd3KJeJEpJ/ATYLZrOKQQ03Cqu1t",
-    "__Secure-3PAPISID": "wn-_6ONd3KJeJEpJ/ATYLZrOKQQ03Cqu1t",
-    "SEARCH_SAMESITE": "CgQIzJ0B",
-    "SID": "g.a000wQiPPGC9lPuLqojiU14B0MAHOU2wQbonkF5AJbeIhUYpw-llxe51WUiQlEdALjxbR2zERwACgYKAbUSARISFQHGX2MiG9reSLuPyy1OUK-IS490aRoVAUF8yKrLkF9O7qnChZUy7Pt2mThP0076",
-    "__Secure-1PSID": "g.a000wQiPPGC9lPuLqojiU14B0MAHOU2wQbonkF5AJbeIhUYpw-llY6-wvFrJpvJ7SV-m1wuImQACgYKASYSARISFQHGX2MibaTGm4xTdj7sKyYEw3eNkhoVAUF8yKquX3DQZx3AUjTezJ74bDh20076",
-    "__Secure-3PSID": "g.a000wQiPPGC9lPuLqojiU14B0MAHOU2wQbonkF5AJbeIhUYpw-ll3KIoV4hJhLj7bducZHaiwQACgYKAYESARISFQHGX2MiHrHcCG6IYzc2Q8AMxhSWQRoVAUF8yKporACGevaSLGxG0x-kc7eK0076",
-    "S": "billing-ui-v3=0vm8rTIKpRDgpj11CvWYsnbaukUu_MAb:billing-ui-v3-efe=0vm8rTIKpRDgpj11CvWYsnbaukUu_MAb",
-    "__Secure-1PSIDTS": "sidts-CjIBjplskOOHqRfyeORGL_g5u6tvigMJhFEjFbyIAhmg24CQPbQ3qHD21qEPeb1JXbxFtRAA",
-    "__Secure-3PSIDTS": "sidts-CjIBjplskOOHqRfyeORGL_g5u6tvigMJhFEjFbyIAhmg24CQPbQ3qHD21qEPeb1JXbxFtRAA",
-    "_gid": "GA1.3.818868622.1747383919",
-    "AEC": "AVcja2eMf9BdHdSI6msBqJeAVp-R4L4o8Rmu3XXm_W5l1BO2eVWdOnqpSQ",
-    "NID": "524=IugAce36U5hOGbHVrJN9cvhbcvMpjAuOZqjknMEpZqOJaq0nNDgt982oOYI0b6E-WmouXLVDUJ8HlV_0-7WqUjPZbngaBhw-ivBnjQj5nROg-lIQgR-4TPVX-S8jh5x-35dJ3Pz6-p6FmtZ-Du-QWbuOYu_Um_wCnraox3Q_eNFZLOY7blny6N7ugGZ_2KNKUBqNCmRbtjWgZjVSY9RhbMHMpDPX0aoEpy0Y_0CYqgaN5YTjdbUpSxi4k_9XhLV1QLnc-DOKnp2FspA-uZ4WigZat0xtFqD1QCQwq07hwGWzs9FNkyvPTqzFmFECCvga8tQVeeNxdGtdtnCbOVFMdzC4pHRt8sYCWulBL34rj684kS4mGBw0rkAsAlmirbXELDgXl3mlDNhGn0ZnbY55osakQYQ8XYO-bd3oWrh9aP6JXTkfEfS6vpMMLR8e2ipJgsaDqAER-1MgJ5k8crUZHzp3L9lhZ7MlSa7KpRLcUd2AAQdk_e49b0nzJtFpQtHKM1c2f-VYoLZ2Q9sJvX8st72QjyR8lH0-IpL5_rZgIPbXZvSFCKNnGtDjS5ZrSq9hw4jGCukSvika9570EcNly73U7wvetHZxyc4Tp7-gbJ4y_SFqlE9wnhQuCfNWVKqJZusAvrYhmlZdiebuvaEpzzzcIGwl",
-    "_gat": "1",
-    "_ga": "GA1.3.147074646.1747383919",
-    "_gat_marketingTracker": "1",
-    "_ga_S4FJY0X3VX": "GS2.1.s1747389947$o2$g1$t1747391495$j0$l0$h0",
-    "SIDCC": "AKEyXzVPv4EFGrL99WVum2u1VxILbdb9PJV8RzM45BJjmznESlKFS7tXsqx2zETmDGmO4V1D_g",
-    "__Secure-1PSIDCC": "AKEyXzXG4BPiUFJQHAjknn5vMeHXtpVpe-_wAlQ2YEI8QTl_MBkOEtVNJGE2rt2JbJ0zyo-_y9M",
-    "__Secure-3PSIDCC": "AKEyXzXyaVGBwLCZfaYVjzp-TkTmforptXDMAi7DrGIoER7vPnvjM0qNhrDEtM_cDXRMxsjVWTo"
-}
-
+cookies = json.loads(os.environ['COOKIES'])
 payload_str = '''
 {
   "dataRequest": [
